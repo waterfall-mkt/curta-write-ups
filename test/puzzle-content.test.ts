d240f4663c8773d224d9457f4400087d102e43a4
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2023-12-02 19:34:40 -0500

    tests: run content tests on all MDX files inside `./puzzles`

--- test/puzzle-content.test.ts
+++ test/puzzle-content.test.ts
@@ -1,63 +1,64 @@
+import { glob } from 'glob';
 import { expect, test } from 'vitest';
 
 import { ELEMENTS } from '@/lib/constants/elements';
-import { readFile, serializeToMDXSource } from '@/lib/utils';
-
-// -----------------------------------------------------------------------------
-// Set up
-// -----------------------------------------------------------------------------
-
-const source = await readFile('./puzzles/eth/4.mdx');
-const mdxSource = await serializeToMDXSource(source);
-const frontmatter = mdxSource.frontmatter;
-
-const p1 = /(_jsxs|_jsx)\(_components.[a-zA-Z-_0-9]+/g;
-const p2 = /(_jsxs|_jsx)\("[a-zA-Z-_0-9]+"/g;
-const p3 = /(_jsxs|_jsx)\([a-zA-Z-_0-9]+/g;
-const pattern = new RegExp(`${p1.source}|${p2.source}|${p3.source}`, 'g');
-const elements = mdxSource.compiledSource.match(pattern) ?? [];
-const uniqueElements = [
-  ...new Set(
-    [...new Set(elements)].map((e) => {
-      let e_ = e;
-      if (e.startsWith('_jsxs(')) e_ = e.substring(6);
-      else if (e.startsWith('_jsx(')) e_ = e.substring(5);
-      if (e_.startsWith('"') && e_.endsWith('"')) e_ = e_.substring(1, e_.length - 1);
-      else if (e_.startsWith('_components.')) e_ = e_.substring(12);
-
-      return e_;
-    }),
-  ),
-];
+import { getUniqueElementsFromMDXSource, readFile, serializeToMDXSource } from '@/lib/utils';
+import { JSX_ELEMENTS_PATTERN } from '@/lib/utils/get-unique-elements-from-mdx-source';
 
 // -----------------------------------------------------------------------------
 // Tests
 // -----------------------------------------------------------------------------
 
-test('Verifies `frontmatter` header exists', () => {
-  expect(frontmatter).toBeDefined();
-  expect(frontmatter.author).toBeDefined();
-  expect(frontmatter.contributors).toBeDefined();
-});
-test('Verifies `frontmatter.author` is an address', () => {
-  expect(frontmatter.author).toMatch(/^0x[0-9a-fA-F]{40}$/);
-});
-test('Verifies `frontmatter.contributors` is an array of addresses', () => {
-  expect(frontmatter.contributors).toBeInstanceOf(Array);
-  expect(frontmatter.contributors.length).greaterThan(0);
-  frontmatter.contributors.forEach((contributor) => {
-    expect(contributor).toMatch(/^0x[0-9a-fA-F]{40}$/);
+// Fetch all MDX files in `./puzzles/*`.
+const files = await glob('./puzzles/**/*.mdx', { ignore: 'node_modules/**' });
+
+// Loop through the list of MDX files found in `./puzzles/*`, and verify each
+// file's contents.
+files.map(async (file) => {
+  const paths = file.split('/');
+  const chain = paths[paths.length - 2];
+  const id = paths[paths.length - 1];
+  const identifier = `${chain}/${id}`.replace('.mdx', '');
+
+  const source = await readFile(file);
+  const mdxSource = await serializeToMDXSource(source);
+  const frontmatter = mdxSource.frontmatter;
+  const uniqueElements = getUniqueElementsFromMDXSource(mdxSource.compiledSource);
+
+  // ---------------------------------------------------------------------------
+  // Verify `frontmatter` content
+  // ---------------------------------------------------------------------------
+
+  test(`[${identifier}]: Verifies \`frontmatter\` header exists`, () => {
+    expect(frontmatter).toBeDefined();
+    expect(frontmatter.author).toBeDefined();
+    expect(frontmatter.contributors).toBeDefined();
   });
-});
-test('Verifies `frontmatter.contributors` to include the primary author', () => {
-  expect(frontmatter.contributors).toContain(frontmatter.author);
-});
-test('Verifies content does not contain any prohibited substrings.', () => {
-  const matches = source.match(pattern);
-  expect(matches === null || matches.length === 0).toBe(true);
-});
-test('Verifies content does not contain any invalid JSX elements.', () => {
-  uniqueElements.forEach((element) => {
-    expect(ELEMENTS).toContain(element);
+  test(`[${identifier}]: Verifies \`frontmatter.author\` is an address`, () => {
+    expect(frontmatter.author).toMatch(/^0x[0-9a-fA-F]{40}$/);
+  });
+  test(`[${identifier}]: Verifies \`frontmatter.contributors\` is an array of addresses`, () => {
+    expect(frontmatter.contributors).toBeInstanceOf(Array);
+    expect(frontmatter.contributors.length).greaterThan(0);
+    frontmatter.contributors.forEach((contributor) => {
+      expect(contributor).toMatch(/^0x[0-9a-fA-F]{40}$/);
+    });
+  });
+  test(`[${identifier}]: Verifies \`frontmatter.contributors\` to include the primary author`, () => {
+    expect(frontmatter.contributors).toContain(frontmatter.author);
+  });
+
+  // ---------------------------------------------------------------------------
+  // Verify MDX content
+  // ---------------------------------------------------------------------------
+
+  test(`[${identifier}]: Verifies content does not contain any prohibited substrings.`, () => {
+    const matches = source.match(JSX_ELEMENTS_PATTERN);
+    expect(matches === null || matches.length === 0).toBe(true);
+  });
+  test(`[${identifier}]: Verifies content does not contain any invalid JSX elements.`, () => {
+    uniqueElements.forEach((element) => {
+      expect(ELEMENTS).toContain(element);
+    });
   });
 });
